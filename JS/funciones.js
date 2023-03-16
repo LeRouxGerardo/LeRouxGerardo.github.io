e6e69ef16614baf41657cf7c051ae344d781ecac
LeRouxGerardo <126741099+LeRouxGerardo@users.noreply.github.com>
2023-03-16 00:33:03 -0300

    Add files via upload
    recuperatorio sin js

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,30 +1,3 @@
-
-document.getElementById('nav_DP').addEventListener('click', function () {
-    document.getElementById('carta-present').style.display = 'none'
-    document.getElementById('form-aca').style.display = 'none'
-    document.getElementById('dat-per').style.display = 'block'
-    document.getElementById('exp-lab').style.display = 'none'
- });
-   
- document.getElementById('nav_FA').addEventListener('click', function () {
-    document.getElementById('carta-present').style.display = 'none'
-    document.getElementById('dat-per').style.display = 'none'
-    document.getElementById('form-aca').style.display = 'block'
-    document.getElementById('exp-lab').style.display = 'none'
- });
-
- document.getElementById('nav_EL').addEventListener('click', function () {
-    document.getElementById('carta-present').style.display = 'none'
-    document.getElementById('dat-per').style.display = 'none'
-    document.getElementById('form-aca').style.display = 'none'
-    document.getElementById('exp-lab').style.display = 'block'
- });
-
- document.getElementById('nav_VI').addEventListener('click', function () {
-    document.getElementById('carta-present').style.display = 'block'
-    document.getElementById('dat-per').style.display = 'none'
-    document.getElementById('form-aca').style.display = 'none'
-    document.getElementById('exp-lab').style.display = 'none'
- });
-
-
+document.getElementsByClassName('n_downl').addeventlister('click', function(){
+    onclick="window.open('Tracy Brown.pdf')"
+})
