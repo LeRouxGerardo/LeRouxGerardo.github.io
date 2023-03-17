887c6bd732d408eaa5a5d1d81215e0f60b78ec99
LeRouxGerardo <126741099+LeRouxGerardo@users.noreply.github.com>
2023-03-17 17:57:34 -0300

    Add files via upload
    commit antes de hacer cambio en el formulario

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,22 @@
-/*document.getElementsByClassName('n_downl').addeventlister('click', function(){
-    onclick="window.open('Tracy Brown.pdf')"
-})
+/*const email = document.getElementById("email");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+  } else {
+    email.setCustomValidity("");
+  }
+});*/
+
+document.getElementById('email').addEventListener('input', function() {
+    campo = event.target;
+    valido = document.getElementById('emailOK');
+        
+    emailRegex = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i
+    ;
+    if (emailRegex.test(campo.value)) {
+      valido.innerText = "válido";
+    } else {
+      valido.innerText = "incorrecto";
+    }
+});
