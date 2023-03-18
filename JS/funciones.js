04914d8f25d768904e869d92b63ac654c7e7da21
LeRouxGerardo <126741099+LeRouxGerardo@users.noreply.github.com>
2023-03-17 23:43:31 -0300

    commit final

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,22 +1,46 @@
-/*const email = document.getElementById("email");
+let id = (id) => document.getElementById(id);
 
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
-  } else {
-    email.setCustomValidity("");
-  }
-});*/
+let classes = (classes) => document.getElementsByClassName(classes);
 
-document.getElementById('email').addEventListener('input', function() {
-    campo = event.target;
-    valido = document.getElementById('emailOK');
-        
-    emailRegex = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i
-    ;
-    if (emailRegex.test(campo.value)) {
-      valido.innerText = "válido";
-    } else {
-      valido.innerText = "incorrecto";
-    }
+let nombre = id("nombre"),
+  apellido = id("apellido"),
+  empresa = id("empresa"),
+  pais = id("pais"),
+  email = id("email"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+// Adding the submit event Listener
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(nombre, 0, "Username cannot be blank");
+  engine(apellido, 1, "Apellido cannot be blank");
+  engine(empresa, 2, "empresa cannot be blank");
+  engine(pais, 3, "pais cannot be blank");
+  engine(email, 4, "Email cannot be blank");
+
 });
+
+// engine function which will do all the works
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+
+    // icons
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+
+    // icons
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
